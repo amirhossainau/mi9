31c17d913c1013031b04ee617a4737a253fce67d
Mohammad Amir Hossain <mdamir.hossain.au@gmail.com>
2016-06-21 21:51:51 +1000

    do all

--- nine9/nine9/Models/ResponseResult.cs
+++ nine9/nine9/Models/ResponseResult.cs
@@ -8,7 +8,11 @@ namespace nine9.Models
     public class ResponseResult
     {
         #region Properties
-        public List<Response> response { get; set; }
+        public List<Response> response { get; private set; }
+        public ResponseResult(List<Payload> payload)
+        {
+            this.response = payload.Where(d => d.drm == true && d.episodeCount > 0).Select(x => new Response { image = x.image.showImage, slug = x.slug, title = x.title }).ToList();
+        }
         #endregion
     }
 }
